b90d1251c02103adc31cb0121dc208dda70afc2d
unknown <lichtgestaltshestakov@yandex.ru>
2022-01-20 19:05:05 +0300

    Добавлен тест на Point..

--- hw/src/test/java/ru/stqa/pft/distance/DistanceTest.java
+++ hw/src/test/java/ru/stqa/pft/distance/DistanceTest.java
@@ -1,11 +1,12 @@
 package ru.stqa.pft.distance;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DistanceTest {
     @Test
     public void testArea(){
         Distance d = new Distance(7, 3);
-        assert d.area() == 6.324555320336759;
+        Assert.assertEquals(d.area(), 6.324555320336759);
     }
 }
